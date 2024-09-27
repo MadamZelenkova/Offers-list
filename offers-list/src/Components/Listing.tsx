cf79562daba3ed0ebab23fc71e166a832e91c5a2
Zelenkova <e.valerevna.turova@yandex.ru>
2024-09-27 16:20:22 +0400

    initial commit

---
+++ offers-list/src/Components/Listing.tsx
@@ -0,0 +1,11 @@
+import Item from "./Item";
+
+export default function Listing({ items = [] }: { items: Array<any> }) {
+  return (
+    <div className="item-list">
+      {items.map((item) =>
+        item.error_messages ? null : <Item key={item.listing_id} item={item} />
+      )}
+    </div>
+  );
+}
---
+++ offers-list/src/Types/ItemType.ts
@@ -0,0 +1,12 @@
+export default interface ItemType {
+  listing_id: number;
+  url: string;
+  MainImage: {
+    url_570xN: string;
+    url_75x75: string;
+  };
+  title: string;
+  currency_code: string;
+  price: string;
+  quantity: number;
+}
